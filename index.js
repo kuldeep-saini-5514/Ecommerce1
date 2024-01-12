3ca848ed3d47f97da9836fd2f324473ede063b5a
Kuldeep Saini <142159571+kuldeep-saini-5514@users.noreply.github.com>
2024-01-12 20:51:33 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,48 +1,31 @@
-const express=require("express");
-const app=express();
-const bodyParser = require('body-parser');
-const cors=require("cors")
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
 require("dotenv").config();
-const PORT=process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-
-//Cors management
 app.use(
   cors({
-      origin:["https://localhost:5000","https://siya-ram-store.vercel.app"],
-      methods: ["post","get"],
-      credentials:true,
+    origin: ["https://localhost:3000", "https://siya-ram-store.vercel.app"],
+    methods: ["post", "get"],
+    credentials: true,
   })
-)
+);
 
-// Middleware to parse JSON and handle form data
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-const fileupload=require("express-fileupload");
-app.use(fileupload(
-    {useTempFiles:true,
-    tempFileDir:'/tmp/'}
-));
+const fileupload = require("express-fileupload");
+app.use(fileupload({ useTempFiles: true, tempFileDir: "/tmp/" }));
 
-//DB connect
-const db=require("./config/database");
+const db = require("./config/database");
 db.connect();
 
-//cloudinary connect 
-const cloudinary=require("./config/cloudinary");
+const cloudinary = require("./config/cloudinary");
 cloudinary.cloudinaryconnect();
 
-//video upload
-const upload=require("./routes/FileUpload");
-app.use('/api/v1/upload',upload);
+const upload = require("./routes/FileUpload");
+app.use("/api/v1/upload", upload);
 
-
-//default route
-app.get("/",(req,res)=>{
-  res.send(`<h1> This is Home page bhaiye </h1>`);
-})
-
-app.listen(PORT,()=>{
-    console.log(`App is running at ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`App is running at ${PORT}`);
+});
